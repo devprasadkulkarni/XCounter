a987d827088c06b4e1259fba4f66f0dbc2e98d99
Prasad Kulkarni <dev.prasad.kulkarni@gmail.com>
2025-10-22 11:10:35 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    const handleClick = (e) => {
+        if (e.target.textContent.includes("Increment")) {
+            setCount((prev) => prev + 1);
+        } else if (e.target.textContent.includes("Decrement")) {
+            setCount((prev) => prev - 1);
+        }
+    };
 
-export default App
+    return (
+        <div className="flex flex-col justify-center items-center h-screen">
+            <h1 className="text-5xl">Counter App</h1>
+            <section className="m-4">Count: {count}</section>
+            <div className="flex gap-4 m-4">
+                <button
+                    onClick={handleClick}
+                    className="px-2 py-2 border cursor-pointer bg-green-400 rounded-md"
+                >
+                    Increment
+                </button>
+                <button
+                    onClick={handleClick}
+                    className="px-2 py-2 border cursor-pointer bg-red-400 rounded-md"
+                >
+                    Decrement
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default App;
